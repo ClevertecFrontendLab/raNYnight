5a10ccaf675ca4266af62239aacc40186859e781
raNYnight <rany4ka@gmail.com>
2024-02-13 11:47:45 +0300

    feat: add footer link

--- src/pages/main-page/layout/footer/footer.tsx
+++ src/pages/main-page/layout/footer/footer.tsx
@@ -1,5 +1,14 @@
+import { Footer as AntdFooter } from 'antd/lib/layout/layout';
+import Link from 'antd/lib/typography/Link';
+
+import './footer.less';
+
 const Footer = () => {
-    return <footer>Footer</footer>;
+    return (
+        <AntdFooter>
+            <Link className='footer-link'>Смотреть отзывы</Link>
+        </AntdFooter>
+    );
 };
 
 export default Footer;
