640dcd82bd3133594de35af7623c7e0527458a6b
Ilya <rany4ka@gmail.com>
2024-02-19 22:45:44 +0300

    feat: comfirm email

--- src/pages/auth-page/forgot-password/forgot-password.tsx
+++ src/pages/auth-page/forgot-password/forgot-password.tsx
@@ -1,40 +1,64 @@
-import { ResultImages, ResultMessages } from '@constants/results';
+import { ResultImages, ResultMessages, ResultTitles } from '@constants/results';
 import { Typography } from 'antd';
 import VerificationInput from 'react-verification-input';
 
 import { useAppSelector } from '@hooks/typed-react-redux-hooks';
 import { selectForgotEmail } from '@redux/auth/authSlice';
 import './forgot-password.less';
+import { useConfirmEmailMutation } from '@redux/auth/authApi';
+import Loader from '@components/loader/loader';
+import { Navigate } from 'react-router-dom';
+import { Paths } from '@router/paths';
 const { Text } = Typography;
 
 const ForgotPassword = () => {
     const forgotEmail = useAppSelector(selectForgotEmail);
 
-    const onComplete = (value: string) => {
-        console.log(value);
+    const [confirmEmail, { isLoading, isError, isSuccess }] = useConfirmEmailMutation();
+
+    const onComplete = async (code: string) => {
+        console.log(code);
+        await confirmEmail({ email: forgotEmail, code });
+        console.log('code sent', { email: forgotEmail, code });
     };
 
+    if (isSuccess) {
+        return <Navigate to={`${Paths.AUTH}/${Paths.CHANGE_PASSWORD}`} />;
+    }
     return (
-        <div className='auth-forgot-wrapper'>
-            <div className='auth-result-image'>{ResultImages.NOTICE}</div>
-            <Text className='auth-forgot-title'>
-                Введите код <br /> для восстановления аккауанта
-            </Text>
-            <Text className='auth-forgot-message'>{`Мы отправили вам на e-mail ${forgotEmail} шестизначный код. Введите его в поле ниже.`}</Text>
-            <VerificationInput
-                validChars='0-9'
-                placeholder=''
-                classNames={{
-                    container: 'verification-input-container',
-                    character: 'verification-input-character',
-                    characterInactive: 'verification-input-character--inactive',
-                    characterSelected: 'verification-input-character--selected',
-                    characterFilled: 'verification-input-character--filled',
-                }}
-                onComplete={onComplete}
-            />
-            <Text className='auth-forgot-message'>{ResultMessages.RESET_CODE_SPAM}</Text>
-        </div>
+        <>
+            <div className={`auth-forgot-wrapper ${isLoading ? 'background-filter' : ''}`}>
+                <div className='auth-result-image'>
+                    {isError ? ResultImages.ERROR : ResultImages.NOTICE}
+                </div>
+                <Text className='auth-forgot-title'>
+                    {isError ? (
+                        ResultTitles.ERROR_BAD_CODE
+                    ) : (
+                        <>
+                            Введите код <br /> для восстановления аккауанта
+                        </>
+                    )}
+                </Text>
+                <Text className='auth-forgot-message'>{`Мы отправили вам на e-mail ${forgotEmail} шестизначный код. Введите его в поле ниже.`}</Text>
+                <VerificationInput
+                    validChars='0-9'
+                    placeholder=''
+                    classNames={{
+                        container: 'verification-input-container',
+                        character: `verification-input-character ${
+                            isError ? 'verification-error' : ''
+                        }`,
+                        characterInactive: 'verification-input-character--inactive',
+                        characterSelected: 'verification-input-character--selected',
+                        characterFilled: 'verification-input-character--filled',
+                    }}
+                    onComplete={onComplete}
+                />
+                <Text className='auth-forgot-message'>{ResultMessages.RESET_CODE_SPAM}</Text>
+            </div>
+            {isLoading && <Loader />}
+        </>
     );
 };
 
