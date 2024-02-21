828037f3664453cb9bdfc7457f19a95d32979a3f
Ilya <rany4ka@gmail.com>
2024-02-21 22:46:58 +0300

    fix: styles

--- src/router/should-redirect.ts
+++
@@ -1,5 +0,0 @@
-export const shouldRedirect = (prevPath: string | undefined, pathToCompare: string) => {
-    if (!prevPath) return true;
-    const normalizedPrevPathname = prevPath.endsWith('/') ? prevPath.slice(0, -1) : prevPath;
-    return pathToCompare != normalizedPrevPathname;
-};
