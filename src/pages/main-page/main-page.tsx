bc30a08ab09d2dfb3a60faac3134ef2aed823a8c
Ilya <rany4ka@gmail.com>
2024-03-27 14:18:18 +0300

    refactor: idk

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,10 +1,10 @@
 import React, { useLayoutEffect } from 'react';
-import { UserData } from '@common-types/profile';
+import Loader from '@components/loader/loader';
 import SidePanel from '@components/side-panel/side-panel';
-import { ApiEndpoints, baseQuery } from '@constants/api';
-import { useAppDispatch, useAppSelector } from '@hooks/typed-react-redux-hooks';
+import { useAppSelector } from '@hooks/typed-react-redux-hooks';
 import { selectAuthToken } from '@redux/auth/auth-slice';
-import { selectUserInfo, setUserInfo } from '@redux/profile/profile-slice';
+import { useLazyGetUserInfoQuery } from '@redux/profile/profile-api';
+import { selectUserInfo } from '@redux/profile/profile-slice';
 import { Layout } from 'antd';
 
 import MainFooter from './layout/footer/footer';
@@ -14,32 +14,10 @@ import MainContent from './layout/main-content/main-content';
 import './main-page.less';
 
 export const MainPage: React.FC = () => {
-    const dispatch = useAppDispatch();
     const userInfo = useAppSelector(selectUserInfo);
     const authToken = useAppSelector(selectAuthToken);
 
-    const getUserInfo = async () => {
-        try {
-            const response = await fetch(`${baseQuery}${ApiEndpoints.UserMe}`, {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${authToken}`,
-                },
-            });
-
-            if (response.ok) {
-                const data: UserData = await response.json();
-
-                dispatch(setUserInfo(data));
-            } else {
-                dispatch(setUserInfo(null));
-            }
-        } catch (error) {
-            dispatch(setUserInfo(null));
-        }
-    };
-
-    // const [getUserInfo, { isLoading }] = useLazyGetUserInfoQuery(); //w8 for solution
+    const [getUserInfo, { isLoading }] = useLazyGetUserInfoQuery();
 
     useLayoutEffect(() => {
         if (!userInfo?.email && authToken) {
@@ -55,6 +33,7 @@ export const MainPage: React.FC = () => {
                 <MainContent />
                 <MainFooter />
             </Layout>
+            {isLoading && <Loader />}
         </Layout>
     );
 };
