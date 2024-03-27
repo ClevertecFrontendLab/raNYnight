a0fe76a055646e9906bc573856b9af3c9d5594b8
Ilya <rany4ka@gmail.com>
2024-03-27 13:35:30 +0300

    fix: temp solution for tests

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
-import Loader from '@components/loader/loader';
 import SidePanel from '@components/side-panel/side-panel';
-import { useAppSelector } from '@hooks/typed-react-redux-hooks';
-import { useLazyGetUserInfoQuery } from '@redux/profile/profile-api';
-import { selectUserInfo } from '@redux/profile/profile-slice';
+import { ApiEndpoints, baseQuery } from '@constants/api';
+import { useAppDispatch, useAppSelector } from '@hooks/typed-react-redux-hooks';
+import { selectAuthToken } from '@redux/auth/auth-slice';
+import { selectUserInfo, setUserInfo } from '@redux/profile/profile-slice';
 import { Layout } from 'antd';
 
 import MainFooter from './layout/footer/footer';
@@ -13,8 +13,32 @@ import MainContent from './layout/main-content/main-content';
 import './main-page.less';
 
 export const MainPage: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const authToken = useAppSelector(selectAuthToken);
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch(`${baseQuery}${ApiEndpoints.UserMe}`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+
+                dispatch(setUserInfo(data));
+            } else {
+                dispatch(setUserInfo(null));
+            }
+        } catch (error) {
+            dispatch(setUserInfo(null));
+        }
+    };
+
     const userInfo = useAppSelector(selectUserInfo);
-    const [getUserInfo, { isLoading }] = useLazyGetUserInfoQuery();
+    // const [getUserInfo, { isLoading }] = useLazyGetUserInfoQuery(); //w8 for solution
 
     useEffect(() => {
         if (!userInfo?.email) {
@@ -23,16 +47,13 @@ export const MainPage: React.FC = () => {
     }, [userInfo, userInfo?.email]);
 
     return (
-        <React.Fragment>
-            <Layout className='page-layout'>
-                <SidePanel />
-                <Layout>
-                    <MainHeader />
-                    <MainContent />
-                    <MainFooter />
-                </Layout>
+        <Layout className='page-layout'>
+            <SidePanel />
+            <Layout>
+                <MainHeader />
+                <MainContent />
+                <MainFooter />
             </Layout>
-            {isLoading && <Loader />}
-        </React.Fragment>
+        </Layout>
     );
 };
